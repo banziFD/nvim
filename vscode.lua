9f3f23686bff29b76eda99b93d7c385b2edcaa2b
banziFD <supengyumail@gmail.com>
2023-11-15 20:22:58 +0800

    use vscode.action instead

--- vscode.lua
+++ vscode.lua
@@ -11,43 +11,33 @@ vim.keymap.set('n', ";", ":", {})
 vim.keymap.set('n', ":", ";", {})
 
 vim.keymap.set('n', "gr", function()
-  vscode.notify('references-view.findReferences')
+  vscode.action('references-view.findReferences')
 end, {})
 
 vim.keymap.set('n', "gi", function()
-  vscode.notify('references-view.findImplementations')
+  vscode.action('references-view.findImplementations')
 end, {})
 
 vim.keymap.set('n', "<leader>rn", function()
-  vscode.notify('editor.action.rename')
+  vscode.action('editor.action.rename')
 end, {})
 
 vim.keymap.set('n', "<leader>fm", function()
-  vscode.notify('editor.action.formatDocument')
+  vscode.action('editor.action.formatDocument')
 end, {})
 
 vim.keymap.set('v', "<leader>fs", function()
-  vscode.notify('workbench.action.findInFiles')
+  vscode.action('workbench.action.findInFiles')
 end, {})
 
 vim.keymap.set('n', "<leader>fs", function()
-  vscode.notify('workbench.action.findInFiles')
+  vscode.action('workbench.action.findInFiles')
 end, {})
 
 vim.keymap.set('n', "<leader>e", function()
-  vscode.notify('workbench.action.toggleSidebarVisibility')
+  vscode.action('workbench.action.toggleSidebarVisibility')
 end, {})
 
 vim.keymap.set('n', "<leader>ff", function()
-  vscode.notify('workbench.action.quickOpen')
+  vscode.action('workbench.action.quickOpen')
 end, {})
-
-
-
--- vim.keymap.set('i', "<c-j>", function()
---   vscode.notify('workbench.action.togglePanel')
--- end,{})
--- vim.keymap.set('n', "<c-j>", function()
---   vscode.notify('workbench.action.togglePanel')
--- end,{})
---
