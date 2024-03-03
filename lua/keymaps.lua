6bd7d4c0c3be7229543126fcfaca800a18e09e81
banziFD <supengyumail@gmail.com>
2024-03-03 09:54:21 +0800

    abort remap ;

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,5 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.keymap.set('n', ";", ":", {})
-vim.keymap.set('n', ":", ";", {})
 vim.keymap.set('n', "<Leader>d", function() 
   vim.diagnostic.open_float(0, {scope = 'line'})
 end, {})
